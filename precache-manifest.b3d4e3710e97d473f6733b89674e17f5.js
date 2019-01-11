77438ae0db31b5b7d6ccdf8b0869c77dff3a51de
Mehdi Abou <aboumehdi.pro@gmail.com>
2019-01-11 17:31:16 +0100

    Updates

---
+++ precache-manifest.b3d4e3710e97d473f6733b89674e17f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b470564821c19dc383cf82898a868e4",
+    "url": "/CME/static/media/logo_ethereum.2b470564.svg"
+  },
+  {
+    "revision": "b266af89cfb2bf65075b",
+    "url": "/CME/static/js/runtime~main.b266af89.js"
+  },
+  {
+    "revision": "dbc8648f6db7661ac709",
+    "url": "/CME/static/js/main.dbc8648f.chunk.js"
+  },
+  {
+    "revision": "48e5e7279c9f35f275c6",
+    "url": "/CME/static/js/1.48e5e727.chunk.js"
+  },
+  {
+    "revision": "dbc8648f6db7661ac709",
+    "url": "/CME/static/css/main.bd755002.chunk.css"
+  },
+  {
+    "revision": "48e5e7279c9f35f275c6",
+    "url": "/CME/static/css/1.3defaf4a.chunk.css"
+  },
+  {
+    "revision": "bbab0ca87ef10e1177533da5cc51a648",
+    "url": "/CME/index.html"
+  }
+];
